8957be726e9213a2fad938776766497e480eecaf
Nicholas Isola da Silva <nicholas.isola@hotmail.com>
2024-06-11 08:02:01 -0300

    Alterando_Código

--- Mod5/TesteEbac/src/mainClass/Main.java
+++ Mod5/TesteEbac/src/mainClass/Main.java
@@ -3,7 +3,9 @@ package mainClass;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		String ambiente = args[0];
+		System.out.println("Hello " + ambiente);
+		//System.out.println("Hello World!");
 
 	}
 
