796ecc0e8d8ecffb7a4706c5f11ed4b5b04a80ca
Nicholas Isola da Silva <nicholas.isola@hotmail.com>
2024-07-08 09:36:57 -0300

    Refresh

--- Mod9/Tarefa_Wrapper/tarefa_wrapper/src/tarefa_wrapper/tarefa.java
+++ Mod9/Tarefa_Wrapper/tarefa_wrapper/src/tarefa_wrapper/tarefa.java
@@ -35,22 +35,27 @@ public class tarefa {
     }
 
 	public static void main(String[] args) {
-		
+		System.out.println("---------------------------------------------------");
         int intValue = 10;
+        System.out.println("Tipo int: " + intValue);
         Integer intWrapper = toInteger(intValue);
-        System.out.println("Tipo Integer: " + intWrapper);
-
+        System.out.println("Tipo Integer Wrapper: " + intWrapper);
+        System.out.println("---------------------------------------------------");
         double doubleValue = 5.5;
+        System.out.println("Tipo double: " + doubleValue);
         Double doubleWrapper = toDouble(doubleValue);
-        System.out.println("Tipo Double: " + doubleWrapper);
-
+        System.out.println("Tipo Double Wrapper: " + doubleWrapper);
+        System.out.println("---------------------------------------------------");
         boolean booleanValue = true;
+        System.out.println("Tipo boolean: " + booleanValue);
         Boolean booleanWrapper = toBoolean(booleanValue);
-        System.out.println("Tipo Boolean: " + booleanWrapper);
-
+        System.out.println("Tipo Boolean Wrapper: " + booleanWrapper);
+        System.out.println("---------------------------------------------------");
         char charValue = 'a';
+        System.out.println("Tipo char: " + charValue);
         Character charWrapper = toCharacter(charValue);
-        System.out.println("Tipo Char: " + charWrapper);
+        System.out.println("Tipo Char Wrapper: " + charWrapper);
+        System.out.println("---------------------------------------------------");
 	}
 
 }
