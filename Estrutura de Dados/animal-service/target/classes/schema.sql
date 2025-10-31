66d92bd0df411e538732537dc4e154e1322f1f4c
Thoks10a <nicholas.isola@hotmail.com>
2025-10-31 08:00:23 -0300

    Update binary class files and modify project configurations to include filtered resources

---
+++ Estrutura de Dados/animal-service/target/classes/schema.sql
@@ -0,0 +1,14 @@
+ -- CREATE TABLE ANIMAL (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     nome_provisorio VARCHAR(255) NOT NULL,
+--     idade_estimada INT NOT NULL,
+--     raca VARCHAR(255) NOT NULL,
+--     data_entrada DATE NOT NULL,
+--     data_adocao DATE,
+--     condicoes_chegada VARCHAR(255) NOT NULL,
+--     nome_recebedor VARCHAR(255) NOT NULL,
+--     data_obito DATE,
+--     porte VARCHAR(255) NOT NULL
+-- );
+
+SELECT * FROM ANIMAL;
